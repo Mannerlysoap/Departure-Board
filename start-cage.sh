2bc30bd5b3ad360b02f84d134fa9943e9d2999b9
Mannerlysoap <86838826+Mannerlysoap@users.noreply.github.com>
2026-03-17 21:22:00 +0100

    Update start-cage.sh

--- start-cage.sh
+++ start-cage.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Start Cage in the background
-cage -d firefox &
+cage -d firefox & sleep 2
 
-# Give it a moment to initialize
-sleep 2
-
-# Apply the 270-degree rotation (Left side up)
 wlr-randr --output HDMI-A-1 --transform 270
